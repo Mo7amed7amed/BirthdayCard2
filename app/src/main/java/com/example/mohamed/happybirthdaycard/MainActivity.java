ca7f1f1fb6d96a6a89e7ddff9a0ad88941dc8984
Mohamed <mohamedhamed77751@gmail.com>
2018-05-04 03:23:32 +0200

    Ay 7aja

--- app/src/main/java/com/example/mohamed/happybirthdaycard/MainActivity.java
+++ app/src/main/java/com/example/mohamed/happybirthdaycard/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.mohamed.happybirthdaycard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt1, txt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt1 = (TextView) findViewById(R.id.mName3);
+        txt2 = (TextView) findViewById(R.id.mFriedName3);
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("name");
+        String friend_name = bundle.getString("friend_name");
+        String greeting =getResources().getString(R.string.greeting);
+        String from =getResources().getString(R.string.from_);
+        txt2.setText(greeting + " " + friend_name);
+        txt1.setText(from + " " + name+" "+"!");
     }
 }
